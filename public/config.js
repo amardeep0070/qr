625d64a658bd24d3fc8f9c39650fae472f0ada7e
Amardeep Singh <amsingh@ebsco.com>
2018-11-02 15:41:55 -0400

    qr

---
+++ public/config.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Amardeep
+ */
+//iffy statement to protect the namespace
+(function() {
+    angular
+        .module("TwitterSearch")
+        .config(Config);
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/welcome", {
+                templateUrl: "views/welcome.view.client.html",
+                controller:"WController",
+                controllerAs :"model"
+            })
+            .when("/search", {
+                templateUrl: "views/search.view.client.html",
+                controller:"searchController",
+                controllerAs :"model"
+            })
+            .when("/sucess", {
+                templateUrl: "views/success.view.client.html",
+                controller:"searchController",
+                controllerAs :"model"
+            })
+            .when("/error", {
+                templateUrl: "views/error.view.client.html",
+                controller:"searchController",
+                controllerAs :"model"
+            })
+            .otherwise({
+                redirectTo: "/welcome"
+            })
+
+    }
+})();
---
+++ serverSide/services/search.service.server.js
@@ -0,0 +1,73 @@
+/**
+ * Created by Amardeep on 02/07/17.
+ */
+module.exports = function (app) {
+    var multer  = require('multer')
+    var upload = multer({ dest: 'uploads/' })
+    var QrCode = require('qrcode-reader');
+    var qr = new QrCode();
+    var fs = require('fs');
+    var Jimp = require("jimp");
+    result = false;
+    app.post('/profile', upload.single('avatar'), function (req, res, next) {
+        // req.file is the `avatar` file
+        // req.body will hold the text
+        // fields, if there were any
+        console.log(req.file.filename)
+        var fileName = req.file.filename
+        var buffer = fs.readFileSync( 'uploads/' + fileName);
+
+        Jimp.read(buffer, function(err, image) {
+            if (err) {
+                console.error(err);
+                // TODO handle error
+            }
+            var qr = new QrCode();
+            qr.callback = function(err, value) {
+                if (err) {
+                    console.error(err);
+                    // TODO handle error
+                    result= false;
+                    res.redirect('/#/error')
+                }
+                console.log(value.result);
+                console.log(value);
+                if(value.result == 'Hello this is a test')
+                    res.redirect('/#/sucess')
+                else  res.redirect('/#/error');
+
+            };
+            qr.decode(image.bitmap);
+
+        });
+    })
+
+
+    //
+    // var abc = function(fileName){
+    //     var buffer = fs.readFileSync( 'uploads/' + fileName);
+    //
+    //     Jimp.read(buffer, function(err, image) {
+    //         if (err) {
+    //             console.error(err);
+    //             // TODO handle error
+    //         }
+    //         var qr = new QrCode();
+    //         qr.callback = function(err, value) {
+    //             if (err) {
+    //                 console.error(err);
+    //                 // TODO handle error
+    //                 result= false;
+    //
+    //             }
+    //             console.log(value.result);
+    //             console.log(value);
+    //             result=  true;
+    //
+    //         };
+    //         qr.decode(image.bitmap);
+    //         return result;
+    //     });
+    // }
+
+}
