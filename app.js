843420f767f49dac8b5d881b10777d54ed530df6
Amardeep Singh <amsingh@ebsco.com>
2018-11-02 21:50:15 -0400

    ad

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
+require('./serverSide/app')(app);
+// app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
